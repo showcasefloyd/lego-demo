723a5646daba5b92e0455616bdfc37ee608076d1
Bruce Wayne <bwayne@batcomputer.local>
2019-08-02 20:56:08 -0400

    Moved all the public HTML to src directory

--- src/js/main.js
+++ src/js/main.js
@@ -1,147 +1,147 @@
-$(function(){
+$(function () {
 	var endAni = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 	var $icon = $('span.hidden-menu-icon');
 	var menuopen = false;
-	preload("img/unlock-glow.png","img/menu-bg-small-blank.gif");
+	preload("img/unlock-glow.png", "img/menu-bg-small-blank.gif");
 
 	/* Adjust bottom footer height */
 	fixFooterHeight();
-	
+
 	/* Start Carousel */
 	$("#ninjago-carousel").owlCarousel({
-	    navigation : true,
-	    singleItem : true,
-	    rewindNav : true,
-	    transitionStyle : "backSlide",
-	    addClassActive : true
-    });
-	
+		navigation: true,
+		singleItem: true,
+		rewindNav: true,
+		transitionStyle: "backSlide",
+		addClassActive: true
+	});
+
 	$('#ninjago-logo').addClass('animated fadeIn');
-	
+
 	/* if window opens small enable check box */
-	if(checkWindowSize(992)){
+	if (checkWindowSize(992)) {
 		$("#offcanvas-menu").removeAttr("disabled");
 	}
-	
+
 	/* Open Mobile Menu On Resize */
-	$(window).resize(function() {
+	$(window).resize(function () {
 		fixFooterHeight();
-		
+
 		/* if window opens small enable check box */
-		if(checkWindowSize(992)){
+		if (checkWindowSize(992)) {
 			$icon.rotate(0);
-			$("#offcanvas-menu").removeAttr("disabled");		
+			$("#offcanvas-menu").removeAttr("disabled");
 		} else {
 			//console.log($icon.css("opacity"));
 			$("#offcanvas-menu").attr("disabled", true);
-		}	 
+		}
 		$('#offcanvas-menu').prop('checked', false);
 	});
-	
-	
-	$( "#offcanvas-menu" ).bind( "change", function() {
-		if(!menuopen){
+
+
+	$("#offcanvas-menu").bind("change", function () {
+		if (!menuopen) {
 			menuopen = true;
-			$icon.rotate(-90);		
+			$icon.rotate(-90);
 		} else {
 			menuopen = false;
 			$icon.rotate(0);
 		}
 	});
-	
+
 	/** Homepage Animations **/
 	/* First Season Button */
-	$('#enterButtonOverlay').on('mouseover',function(){
+	$('#enterButtonOverlay').on('mouseover', function () {
 		$next = $('#enterButtonOverlay').next();
-		runAnimation($next,'pulse');
-		runAnimation('#enterButtonOverlay','tada');
+		runAnimation($next, 'pulse');
+		runAnimation('#enterButtonOverlay', 'tada');
 	});
-	
-	
+
+
 	/* Second Season Button */
 	var buttonUnlock = true;
-	$('#unlockOverlay1').on('mouseover',function(){
-		if(buttonUnlock){
-			runAnimation('#unlockOverlay1 #lock','swing');
-		}			
+	$('#unlockOverlay1').on('mouseover', function () {
+		if (buttonUnlock) {
+			runAnimation('#unlockOverlay1 #lock', 'swing');
+		}
 	});
-	
-	$('#unlockOverlay1').on('click',function(){
-		
+
+	$('#unlockOverlay1').on('click', function () {
+
 		buttonUnlock = false;
-		
-		$("#unlockOverlay1 #lock").animate({'opacity':1},500,function(){
-			
-			$("#unlockOverlay1 #lock img").fadeOut(10,function() { 
-				
-			  	 	$(this).attr('src', 'img/unlock-glow.png').fadeIn(1000).addClass('poplock2').one(endAni, function(){
-					
+
+		$("#unlockOverlay1 #lock").animate({ 'opacity': 1 }, 500, function () {
+
+			$("#unlockOverlay1 #lock img").fadeOut(10, function () {
+
+				$(this).attr('src', 'img/unlock-glow.png').fadeIn(1000).addClass('poplock2').one(endAni, function () {
+
 					$('#unlockOverlay1').addClass('animated slideOutDown');
-					
-					$('#enterButtonOverlay1').on('mouseover',function(){		
+
+					$('#enterButtonOverlay1').on('mouseover', function () {
 						$next = $('#enterButtonOverlay1').next();
-						runAnimation($next,'pulse');
-						runAnimation('#enterButtonOverlay1','bounce');
+						runAnimation($next, 'pulse');
+						runAnimation('#enterButtonOverlay1', 'bounce');
 					});
-						
+
 				});;
-			}); 
-			
+			});
+
 		});
-		
+
 
 	});
-	
+
 	/* Third Season Button */
-	$('#lockOverlay3').on('mouseover',function(){
+	$('#lockOverlay3').on('mouseover', function () {
 		//runAnimation('#lockOverlay3 #lock','shake');
-		$(this).runAnimation('shake','#lock');
+		$(this).runAnimation('shake', '#lock');
 	})
-	
+
 	/* Fourth Season Button */
-	$('#lockOverlay4').on('mouseover',function(){
+	$('#lockOverlay4').on('mouseover', function () {
 		//runAnimation('#lockOverlay4 #lock','shake');
-		$(this).runAnimation('shake','#lock');
+		$(this).runAnimation('shake', '#lock');
 	})
-	
-	
+
+
 	/* Utilies */
-	function runAnimation(selector,ani){
+	function runAnimation(selector, ani) {
 		//console.log("Mouse Over" + selector + " " + ani);
-		$(selector).addClass('animated '+ ani).one(endAni, function(){
-			$(this).removeClass('animated '+ ani);
-		});	
-		
+		$(selector).addClass('animated ' + ani).one(endAni, function () {
+			$(this).removeClass('animated ' + ani);
+		});
+
 		return;
 	}
 
 	// Carousel Events
 	var $activelink = "";
 	var $clickableDiv = $('.ninjago-href');
-	$clickableDiv.on('mouseover',function(){
-		$activelink =  $('#ninjago-carousel .owl-wrapper-outer .owl-wrapper .owl-item.active').find('.item').data('urllink');
+	$clickableDiv.on('mouseover', function () {
+		$activelink = $('#ninjago-carousel .owl-wrapper-outer .owl-wrapper .owl-item.active').find('.item').data('urllink');
 		//console.log($activelink);	
 	});
-	$clickableDiv.on('click',function(){
-		if($activelink != null){
-			window.open($activelink,"_blank");
+	$clickableDiv.on('click', function () {
+		if ($activelink != null) {
+			window.open($activelink, "_blank");
 		}
-	});     
+	});
 });
 
 
 function fixFooterHeight() {
-    var $h = $("footer").height();
-    $('#footer-wrap').height($h * 2);
+	var $h = $("footer").height();
+	$('#footer-wrap').height($h * 2);
 }
-	
-	
+
+
 function checkWindowSize(size) {
-   var pageWidth = $(window).width();  
-   if ( pageWidth <= size ) {
-   		return true;
-   }
-   return false;
+	var pageWidth = $(window).width();
+	if (pageWidth <= size) {
+		return true;
+	}
+	return false;
 }
 
 function preload() {
@@ -151,42 +151,42 @@ function preload() {
 		images[i].src = preload.arguments[i]
 	}
 }
-	
-	
-/* JQuery Functions */	
-jQuery.fn.rotate = function(degrees) {
-	
+
+
+/* JQuery Functions */
+jQuery.fn.rotate = function (degrees) {
+
 	var rotate = "rotate(" + degrees + "deg)";
-    var trans = "all 0.3s ease-out";
+	var trans = "all 0.3s ease-out";
 	$(this).css({
-                "-webkit-transform": rotate,
-                "-moz-transform": rotate,
-                "-o-transform": rotate,
-                "msTransform": rotate,
-                "transform": rotate,
-                "-webkit-transition": trans,
-                "-moz-transition": trans,
-                "-o-transition": trans,
-                "transition": trans});
+		"-webkit-transform": rotate,
+		"-moz-transform": rotate,
+		"-o-transform": rotate,
+		"msTransform": rotate,
+		"transform": rotate,
+		"-webkit-transition": trans,
+		"-moz-transition": trans,
+		"-o-transition": trans,
+		"transition": trans
+	});
 
-    return $(this);
+	return $(this);
 };
 
 
-jQuery.fn.runAnimation = function(animation, subselector) {
-	
-	var endAni = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'	;
-	
+jQuery.fn.runAnimation = function (animation, subselector) {
+
+	var endAni = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
 	if (typeof subselector !== 'undefined') {
-		$(this).find(subselector).addClass('animated '+ animation).one(endAni, function(){
-			$(this).removeClass('animated '+ animation);
-		}); 
+		$(this).find(subselector).addClass('animated ' + animation).one(endAni, function () {
+			$(this).removeClass('animated ' + animation);
+		});
 	} else {
-		$(this).addClass('animated '+ animation).one(endAni, function(){
-			$(this).removeClass('animated '+ animation);
-		});	
+		$(this).addClass('animated ' + animation).one(endAni, function () {
+			$(this).removeClass('animated ' + animation);
+		});
 	}
-		
-    return $(this);
+
+	return $(this);
 };
-			
